52b0733029a0a009cd82938d523e9ef92e886466
junha-lee <44857783+junha-lee@users.noreply.github.com>
2023-04-24 15:58:32 +0900

    add 이준하 백준 크게만들기

---
+++ 4.24/이준하_백준_2812_크게_만들기.py
@@ -0,0 +1,14 @@
+import sys
+input=sys.stdin.readline
+
+N, K = map(int, input().split())
+num = list(input())
+stack = []
+
+for i in num:
+    while(K > 0 and stack and stack[-1] < i):
+        stack.pop()
+        K-=1
+    stack.append(i)
+    
+print(''.join(stack))
