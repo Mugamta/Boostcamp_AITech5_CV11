4ccce965823ac98344f3048ce296345d4eabc1a2
guk98 <78603611+guk98@users.noreply.github.com>
2023-04-25 13:43:11 +0900

    Update 최지욱_백준_1541_잃어버린_괄호.py

--- 4.26/최지욱_백준_1541_잃어버린_괄호.py
+++ 4.26/최지욱_백준_1541_잃어버린_괄호.py
@@ -4,8 +4,7 @@ def main():
     arr = string.split('-')     ## 첫 - 부호로 head와 tail을 분리
     
     head = list(map(int, arr[0].split('+'))) ## head 부분 +로 모두 분리
-    tail = arr[1:]
-    tail = [sum(list(map(int,i.split('+')))) for i in tail]     ## -로 분리된 각 리스트들을 한번 더 +로 분리하고 sum을 구함
+    tail = [sum(list(map(int,i.split('+')))) for i in arr[1:]]     ## -로 분리된 각 리스트들을 한번 더 +로 분리하고 sum을 구함
     
     print(sum(head)-sum(tail))      ## head의 모든 숫자의 합에서 tail의 모든 숫자의 합을 빼기
     
