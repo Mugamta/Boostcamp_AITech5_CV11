503ec33de3b5508605aa3422fd3d1eef8da02979
mugamta <tjwlgns02@gmail.com>
2023-09-12 02:59:02 +0900

    2023.09.12 서지훈 (py)

---
+++ 9.12/서지훈_백준_2011_암호코드.py
@@ -0,0 +1,23 @@
+def func():
+    s = "0" + input()
+
+    if s[1] == '0':
+        return 0
+
+    length = len(s)
+
+    dp = [0 for _ in range(len(s))]
+    dp[0] = 1
+    dp[1] = 1
+
+    for i in range(2, length):
+        if int(s[i]) > 0:
+            dp[i] = dp[i - 1]
+        if 10 <= int(s[i - 1:i + 1]) <= 26:
+            dp[i] += dp[i - 2]
+
+    print(dp)
+    return dp[length - 1] % 1000000
+
+
+print(func())
