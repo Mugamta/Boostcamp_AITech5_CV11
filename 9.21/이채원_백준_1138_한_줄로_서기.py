af191e1f2fbdd555d859c3e306b454c294297455
Chaewon Lee <lcwon9@gmail.com>
2023-09-20 22:17:30 +0900

    add file

---
+++ 9.21/이채원_백준_1138_한_줄로_서기.py
@@ -0,0 +1,25 @@
+
+
+import sys
+N = int(input())
+arr = list(map(int, sys.stdin.readline().split()))
+
+ans = list(False for _ in range(N))
+
+for i in range(N) :
+    k = arr[i] #왼쪽의 키큰 사람 수 
+    temp = 0
+    for j in range(N) :
+        if ans[j] == False :
+            if temp == k :
+                break
+            else : temp += 1
+        else : 
+            pass
+        # print(f"i={i+1},j={j},temp={temp},ans={ans}")
+
+    
+    ans[j] = str(i+1)
+
+print(' '.join(ans))
+# print(ans)
