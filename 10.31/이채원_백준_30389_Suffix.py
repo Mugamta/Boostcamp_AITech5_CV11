c99f7d7e997755c08d85193d1f59fb446e74d4dd
Chaewon Lee <lcwon9@gmail.com>
2023-10-31 09:28:37 +0900

    add file

---
+++ 10.31/이채원_백준_30389_Suffix.py
@@ -0,0 +1,26 @@
+import sys
+
+
+N = int(input())
+S = []
+for _ in range(N) :
+    S.append(input())
+
+temp = [[],[]]
+for i in range(N) :
+    temp[i%2] = []
+    if len(S[i]) == 1  :
+        if S[i] not in temp[(i+1)%2] :
+            temp[i%2].append(S[i])
+    else :        
+        for j in range(len(S[i])) :
+            s = S[i][len(S[i])-j-1:]
+            if s not in temp[(i+1)%2] :
+                temp[i%2].append(s)
+            # print(f"word = {S[i]}, s={s}, temp={temp}")
+print(len(temp[(N+1)%2]))
+
+    
+
+
+    
