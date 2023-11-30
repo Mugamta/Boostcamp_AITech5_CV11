1bbadc308dcb335f27064f0e5a244ec094b5c33f
Chaewon Lee <lcwon9@gmail.com>
2023-11-30 09:31:25 +0900

    add file

---
+++ 11.30/이채원_백준_2156_포도주_시식.py
@@ -0,0 +1,24 @@
+import sys
+
+def solution() :
+    N = int(input())
+    wine = []
+    for _ in range(N) :
+        wine.append(int(input()))
+
+
+    dp = list(0 for _ in range(N))
+    dp[0] = wine[0]
+    dp[1] = wine[0] + wine[1]
+    dp[2] = max([wine[0]+wine[1], wine[0]+wine[2], wine[1]+wine[2]])
+    if N <3 : return(dp[1]) 
+    
+    for i in range(3, N) : 
+        temp1 = dp[i-3] + wine[i-1] + wine[i]
+        temp2 = dp[i-2] + wine[i]
+        temp3 = dp[i-1]
+        dp[i] = max([temp1,temp2,temp3])
+        
+    return dp[N-1]
+
+print(solution())
