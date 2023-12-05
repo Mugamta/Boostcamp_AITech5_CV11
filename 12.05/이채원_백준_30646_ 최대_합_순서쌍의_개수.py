47a2c43a0305594789edd6bca14a479862fcd910
Chaewon Lee <lcwon9@gmail.com>
2023-12-05 09:43:36 +0900

    fix file

--- 12.05/이채원_백준_30646_ 최대_합_순서쌍의_개수.py
+++ 12.05/이채원_백준_30646_ 최대_합_순서쌍의_개수.py
@@ -1,6 +1,6 @@
 import sys
 from collections import defaultdict
-# 3%에서 시간 초과
+
 def solution():
     N = int(input())
     arr = list(map(int, sys.stdin.readline().split()))
@@ -14,10 +14,7 @@ def solution():
     mx = 0  #최대 합
     num = 0 #순서쌍 개수
 
-
     for n in d.keys() :
-        # for i in range(d[n][0], d[n][-1]+1) :
-        #     temp += arr[i]
         temp = ss_arr[d[n][-1]] - ss_arr[d[n][0]] + arr[d[n][0]]
         
         if temp == mx :
