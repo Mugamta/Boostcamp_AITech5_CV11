4ae4325fff4a5da6cf61ab33ddbf6ceff4cdc8e0
guk98 <guk987@naver.com>
2024-08-03 10:54:41 +0900

    add file

---
+++ 2024/08.03/최지욱_프로그래머스_롤케이크_자르기.py
@@ -0,0 +1,32 @@
+'''
+Worst case
+메모리 : 57.2MB
+시간 : 551.10ms 
+'''
+
+def solution(topping):
+    dict = dict()
+    for num in topping:
+        if num not in dict:
+            dict[num] = 1
+        else:
+            dict[num] += 1
+    
+    n = len(dict)
+    
+    cnt =0 
+    set2 = set()
+    for num in topping[::-1]:
+        set2.add(num)
+        dict[num] -= 1
+        if dict[num] ==0:
+            n -= 1
+        
+        if len(set2)==n:
+
+            cnt += 1
+        
+        if len(set2)>n:
+            break
+    
+    return cnt
