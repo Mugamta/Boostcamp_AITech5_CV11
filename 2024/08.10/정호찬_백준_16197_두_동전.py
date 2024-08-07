60344ac337d18d6a593ffd90b2daf25d7fcf7455
정호찬 <108447906+Eumgill98@users.noreply.github.com>
2024-08-07 18:48:37 +0900

    add : 두 동전

---
+++ 2024/08.10/정호찬_백준_16197_두_동전.py
@@ -0,0 +1,58 @@
+"""
+    PyPy3 로 제출
+    메모리 : 267288 KB
+    시 간 : 412 MS
+"""
+
+from collections import deque
+import sys
+
+input = sys.stdin.readline
+
+dx = [-1, 0, 1, 0]
+dy = [0, -1, 0, 1]
+
+def bfs():
+    while coin:
+        x1, y1, x2, y2, cnt = coin.popleft()
+
+        if cnt >= 10:
+            return -1
+
+        for i in range(4):
+            nx1 = x1 + dx[i]
+            ny1 = y1 + dy[i]
+            nx2 = x2 + dx[i]
+            ny2 = y2 + dy[i]
+
+            if 0 <= nx1 < n and 0 <= ny1 < m and 0 <= nx2 < n and 0 <= ny2 < m:
+                # 벽
+                if board[nx1][ny1] == "#":
+                    nx1, ny1 = x1, y1
+                if board[nx2][ny2] == "#":
+                    nx2, ny2 = x2, y2
+                coin.append((nx1, ny1, nx2, ny2, cnt + 1))
+            elif 0 <= nx1 < n and 0 <= ny1 < m:  # coin2가 out
+                return cnt + 1
+            elif 0 <= nx2 < n and 0 <= ny2 < m:  # coin1가 out
+                return cnt + 1
+            else:  # 둘 다  out
+                continue
+
+    return -1
+
+if __name__ == "__main__":
+    n, m = map(int, input().split())
+
+    coin = deque()
+    board = []
+    temp = []
+    for i in range(n):
+        board.append(list(input().strip()))
+        for j in range(m):
+            if board[i][j] == "o":
+                temp.append((i, j))
+
+    coin.append((temp[0][0], temp[0][1], temp[1][0], temp[1][1], 0))
+
+    print(bfs())
