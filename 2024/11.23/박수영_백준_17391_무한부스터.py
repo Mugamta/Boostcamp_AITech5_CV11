81a319fc0b53ffd9b2ad17e71592db10d5b7db06
nstalways <nstalways@gmail.com>
2024-11-22 20:28:12 +0900

    메모리34052KB_시간312ms

--- 2024/11.23/박수영_백준_17391_무한부스터.py
+++ 2024/11.23/박수영_백준_17391_무한부스터.py
@@ -1,12 +1,8 @@
 """
-- BFS >> 최단 경로 탐색 시 사용하는 알고리즘
-- 본 문제의 목표는 격자의 개수 최소화 >> 최단 경로 탐색만으로 만족할 수 없음
-- 중복 탐색을 어떻게 해결할 것인가?
-    - 원래는 visited 배열을 사용하는데, 본 문제에선 격자의 개수를 최소화해야 하기에
-        단순 visited 배열로 판별할 수 없음 >> DP의 필요
-    - DP[i][j]: (i, j) 칸에 도달하기 위해 거친 최소 격자 개수
-    - 만약 (i, j) 칸에 도착했을 때, dp[i][j]보다 거친 격자 개수가 작다면 dp 배열을 갱신하고 추가 탐색
-    - 격자 개수가 크다면 추가 탐색 X
+BFS
+- 최단 경로 탐색
+- 목적지에 도달하기 위해 이동한 최소 격자의 개수를 구해야 함 >> 최단 경로랑 똑같음
+- 최소 격자의 개수는 dp를 사용해서 기록해도 되고, queue에 담는 방식으로 기록해도 됨
 """
 import sys
 from collections import deque
@@ -14,13 +10,15 @@ input = sys.stdin.readline
 
 def solution(n_row, n_col, arr):
     move = [(0, 1), (1, 0)]
-    dp = [[float('inf')] * n_col for _ in range(n_row)]
+    v = [[False] * n_col for _ in range(n_row)]
 
-    queue = deque([(0, 0)])
-    dp[0][0] = 1
+    queue = deque([(0, 0, 0)])
+    v[0][0] = True
 
     while queue:
-        r, c = queue.popleft()
+        r, c, cnt = queue.popleft()
+        if (r, c) == (n_row-1, n_col-1):
+            return cnt
 
         for booster in range(1, arr[r][c] + 1):
             for dr, dc in move:
@@ -31,14 +29,14 @@ def solution(n_row, n_col, arr):
                 if nr >= n_row or nc >= n_col:
                     continue
 
-                # 돌아왔다면 무시
-                if dp[nr][nc] <= dp[r][c] + 1:
+                # 방문했다면 무시
+                if v[nr][nc]:
                     continue
 
-                dp[nr][nc] = dp[r][c] + 1
-                queue.append((nr, nc))
-
-    return dp[n_row-1][n_col-1] - 1
+                queue.append((nr, nc, cnt+1))
+                v[nr][nc] = True
+                
+    return
 
 
 def main():
